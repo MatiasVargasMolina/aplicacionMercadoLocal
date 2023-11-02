719a1de8636caced48a0f35dd1292dc83a055ef8
michimalonco <vargas.mati1999@gmail.com>
2023-11-02 14:29:48 -0300

    doble scrollview

--- AwesomeProject/components/ProductoCard.jsx
+++ AwesomeProject/components/ProductoCard.jsx
@@ -1,12 +1,18 @@
 import React from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
 
-const ProductoCard = ({ producto }) => {
+const { width } = Dimensions.get('window');
+
+const ProductoCard = ({ producto,styke }) => {
   return (
-    <View style={styles.card}>
-      <Image style={styles.imagen} source={{ uri: producto.images[0].src }} />
-      <Text style={styles.titulo}>{producto.name}</Text>
-      <Text style={styles.precio}>Precio: ${producto.price}</Text>
+    <View style={{ ...styles.card, width: width - 20 }}>
+      <View style={styles.leftContainer}>
+        <Image style={styles.imagen} source={{ uri: producto.images[0].src }} />
+      </View>
+      <View style={styles.rightContainer}>
+        <Text style={styles.titulo}>{producto.name}</Text>
+        <Text style={styles.precio}>${producto.price}</Text>
+      </View>
     </View>
   );
 };
@@ -18,10 +24,18 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     padding: 10,
     margin: 10,
-    alignItems: 'center',
+    flexDirection: 'row',
+  },
+  leftContainer: {
+    flex: 1, // Ocupa la mitad del contenedor
+  },
+  rightContainer: {
+    flex: 1, // Ocupa la otra mitad del contenedor
+    marginLeft: 10, // Espacio entre la imagen y el texto
+    justifyContent: 'center', // Centra verticalmente el texto
   },
   imagen: {
-    width: 200,
+    width: '100%', // La imagen ocupará todo el espacio disponible en el contenedor
     height: 200,
     resizeMode: 'cover',
     borderRadius: 10,
@@ -32,7 +46,7 @@ const styles = StyleSheet.create({
     marginVertical: 10,
   },
   precio: {
-    fontSize: 16,
+    fontSize: 40,
     color: 'green',
   },
 });
